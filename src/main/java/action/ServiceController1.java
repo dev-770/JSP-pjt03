4af460f7f1af3530e3f51d771d496fdaec359da9
haneu <rokap11111@gmail.com>
2021-05-23 19:47:16 +0900

    이동 페이지 [Controller]

---
+++ src/main/java/action/ServiceController1.java
@@ -0,0 +1,24 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class ServiceController1 implements Controller {
+	
+	@Override
+	public String handlePost(HttpServletRequest req, HttpServletResponse resp) {
+		return process(req, resp);
+	}
+
+	@Override
+	public String handleGet(HttpServletRequest req, HttpServletResponse resp) {
+		return process(req, resp);
+	}
+	
+	private String process(HttpServletRequest req, HttpServletResponse resp) {
+		return "customer_service_noitce";
+	}
+
+}
